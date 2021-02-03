2768505fc3c23b1135f0619ffcfbbc89e14548d1
Henrik <henrik@warfvinge.se>
2021-02-03 15:19:32 +0000

    Finished dataflow python pipeline and notebook

--- dataflow-job.py
+++ dataflow-job.py
@@ -15,15 +15,38 @@ def filtering(record):
 
 pipeline1 = beam.Pipeline(options=options)
 
+table_spec = 'crazy-hippo-01:department_dataflow.group_by_name'
+
+table_schema = 'name:STRING, count:INTEGER'
+
+
 #with beam.Pipeline() as pipeline1:
 dep_data_count = (
     pipeline1
       |'Read from file' >> beam.io.ReadFromText('gs://crazy-hippo-01/dataflow_beam_data/dept-data.txt')
       |'Select_data' >> beam.Map(selectData)
       |'Filter record on Accounts' >> beam.Filter(filtering)
-      |'Create Tuple of Records' >> beam.Map(lambda record : (record[1], 1))
+      |'Create Dict of Records' >> beam.Map(lambda record : {"name" : record[1], "count" : 1})
       |'Apply CombinePerKey on Records' >> beam.CombinePerKey(sum)
-      |'Write to Cloud Storage' >> beam.io.WriteToText('gs://crazy-hippo-01/dataflow_beam_data/output_new')
+      #|'Make into Dict' >> beam.Map(lambda x: {"name": x[0], "count": x[1]})
+      #|'Write to Cloud Storage' >> beam.io.WriteToText('gs://crazy-hippo-01/dataflow_beam_data/output_new')
+      |'Write to BQ' >> beam.io.WriteToBigQuery(
+                                                table_spec,
+                                                schema=table_schema,
+                                                write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+                                                create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
 )
+
+#dep_data_count | beam.io.WriteToBigQuery(
+ #   table_spec,
+ #   schema=table_schema,
+ #   write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+ #   create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
+
+
+#dep_data_count | beam.io.gcp.bigquery.WriteToBigQuery(table_spec,
+#                                         schema=table_schema,
+#                                         write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+#                                         create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
 
 pipeline1.run().wait_until_finish()
